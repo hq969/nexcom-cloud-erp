7a6a63f925044431a400891a8501f317da46bc35
Harsh Sonkar <114162367+hq969@users.noreply.github.com>
2025-07-23 15:07:50 +0530

    Create db.js

---
+++ shared/utils/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (uri) => {
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+};
+
+module.exports = connectDB;
